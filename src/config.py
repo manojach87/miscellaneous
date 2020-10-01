b68bc0e22b9340492fe74f92b5e2417694bd752d
manojach87 <manoj.ach@gmail.com>
2020-09-30 23:25:03 -0400

    new file:   config.py 	new file:   create_folds.py 	new file:   lbl_xgb.py 	new file:   lbl_xgb_num.py 	new file:   ohe_logres.py

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# config.py
+TRAINING_FILE_NM="adult"
+TRAINING_FILE = "../input/"+TRAINING_FILE_NM+".csv"
+TRAINING_FOLD_FILE = "../input/"+TRAINING_FILE_NM+"_folds.csv"
+
+TEST_FILE_NM="adult-test"
+TEST_FILE = "../input/"+TEST_FILE_NM+".csv"
+
+MODEL_OUTPUT = "../models/"
---
+++ src/create_folds.py
@@ -0,0 +1,28 @@
+
+import os
+import config
+# import pandas and model_selection module of scikit-learn
+import pandas as pd
+from sklearn import model_selection
+
+def createFolds(label="label"):
+    # Training data is in a csv file called train.csv
+    df = pd.read_csv(config.TRAINING_FILE)
+    # we create a new column called kfold and fill it with -1
+    df["kfold"] = -1
+    # the next step is to randomize the rows of the data
+    df = df.sample(frac=1).reset_index(drop=True)
+    # fetch targets
+    #y = df.label.values
+    y = df[label].values
+    y = df[label].values
+    # initiate the kfold class from model_selection module
+    kf = model_selection.StratifiedKFold(n_splits=5)
+    # fill the new kfold column
+    for f, (t_, v_) in enumerate(kf.split(X=df, y=y)):
+        df.loc[v_, 'kfold'] = f
+    # save the new csv with kfold column
+    df.to_csv(config.TRAINING_FOLD_FILE, index=False)
+
+if __name__ == "__main__":
+    createFolds("income")
